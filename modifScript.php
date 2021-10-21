556a873ff41dca993e70f34ebda61fe014562fff
Valentin Muller <valmuller14@gmail.com>
2021-10-21 12:12:26 +0200

    redirection vers l'index.php

--- modifScript.php
+++ modifScript.php
@@ -2,13 +2,12 @@
     include_once './config/login.php';
 ?>
 
-<p><?php echo $_GET['id'] ?></p>
-<p><?php echo $_GET['salaire'] ?></p>
 <?php 
-
 
    $req = "UPDATE employes SET salaire='".$_GET['salaire']."' WHERE id_employes=". $_GET['id'];
    $bdd->query($req);
+
+   header('Location: /');
 
    
 
